2ada4d74eb3fd9a6481ed41567ba2d7eb235f400
Saifkh122 <56623003+Saifkh122@users.noreply.github.com>
2022-01-29 09:04:35 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_FeNclUtnf', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1e5v6v4l0gm75qpqacv67c777n', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_safSad6Gc', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '49dip9mk6t0th9hjuokb7dn5ud', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
